316eaf8398376fa6d127437860a001b51ef2ed04
geosunsetmoth <103338841+geosunsetmoth@users.noreply.github.com>
2023-04-13 15:45:42 -0300

    Main Menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenuUI : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
+    private string playerName;
+    public TMP_Text nameDisplay; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +18,20 @@ public class StartMenuUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (nameDisplay != null)
+        {
+            nameDisplay.SetText(playerName);
+        }
     }
 
     public void GoToGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ReadName(string s)
+    {
+        playerName = s;
+        Debug.Log(playerName);
     }
 }
